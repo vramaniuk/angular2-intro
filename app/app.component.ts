a7696d6df031494c9b542f72cb341680341099f5
viktor.romanyuk <viktor.romanyuk@aisnovations.com>
2017-10-21 19:58:57 +0300

    Angular2 todo6 finished

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+interface ITodo {
+    title: string;
+    completed: boolean
+}
+const todos = [
+    {
+        title: 'Learn JavaScript',
+        completed: true
+    },
+    {
+        title: 'Learn Angular',
+        completed: false
+    },
+    {
+        title: 'Create App',
+        completed: false
+    }
+];
+
 @Component({
-    selector:'app',
-    template: '<h1>Angular 2Do</h1>'
-})
+    moduleId: module.id,
+    selector: 'app',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
+})  
 
-export class AppComponent{
-
+export class AppComponent {
+    title: string = 'Angular 2Do';
+    todos: ITodo[] = todos;
+    toggle(todo: ITodo) {
+        todo.completed = !todo.completed;
+    }
+    delete(todo: ITodo) {
+        let index = this.todos.indexOf(todo);
+        if (index > -1) todos.splice(index, 1)
+    }
 }
