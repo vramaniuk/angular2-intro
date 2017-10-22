0a455d8be4de64c8d063df41bfd32dc78bc9be97
viktor.romanyuk <viktor.romanyuk@aisnovations.com>
2017-10-22 14:44:38 +0300

    finished 7

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-interface ITodo {
-    title: string;
-    completed: boolean
+class ITodo {
+    constructor(
+        public title: string,
+        public completed: boolean = false) { }
 }
 const todos = [
     {
@@ -24,16 +25,22 @@ const todos = [
     selector: 'app',
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.css']
-})  
+})
 
 export class AppComponent {
     title: string = 'Angular 2Do';
     todos: ITodo[] = todos;
+    newTodoTitle: string = '';
     toggle(todo: ITodo) {
         todo.completed = !todo.completed;
     }
     delete(todo: ITodo) {
         let index = this.todos.indexOf(todo);
         if (index > -1) todos.splice(index, 1)
+    }
+    create() {
+        let todo: ITodo = new ITodo(this.newTodoTitle);
+        this.todos.push(todo);
+        this.newTodoTitle = "";
     }
 }
